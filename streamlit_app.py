bbf654b8d665219fb68ea948e4f941b211c0e544
jnhr <jnhr@cowi.com>
2026-03-08 13:36:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,14 @@ with col2:
      sp_max = st.number_input("Max. Span [m]:", min_value=0.0, value=16.0, step=0.5, format="%.2f")
 with col3:
      sp_step = st.number_input("no. steps [-]:", min_value=2, value=20, step=1, format="%i")
+
+
+col1, col2 = st.columns([1, 2])
+with col1:
+     ti = st.number_input("Time frame [s]:", min_value=0, max_value=7200, value=3600, step=100)
+with col2:
+    data = st.multiselect(
+        "Select (multiple) datasets for plot below",
+        datFiles,
+        default=datFiles
+        )
