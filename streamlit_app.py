0561a4d3b5045d1c15a97d4347335c151e96670e
jnhr <jnhr@cowi.com>
2026-03-08 13:45:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,72 @@ import glob
 import matplotlib.pyplot as plt
 import numpy as np
 
-import FirePackage as fp
+def fdflect(Time=3600, 
+            datafile='Data/Ex32.dat',
+            #Height = 0.32,
+            span = 8,
+            Total_points=1200,
+            FS=[],
+            ):
+    
+    Height = float(datafile.split('Ex')[1].split('.')[0])/100
+    #datafile.split('Ex')
+
+    if len(FS) < 1:
+        FS=[(0, span)]
+        
+    data = np.genfromtxt(datafile, names=True)
+    
+    f_bot = interp1d(data['Time'], data['Bottom'], 'cubic')
+    f_top = interp1d(data['Time'], data['Top'], 'cubic')
+    
+    delta_strain = f_bot(Time)-f_top(Time)
+    
+    R = Height/delta_strain
+       
+    sp=np.linspace(0, span, Total_points)
+    for i in FS:
+        sp=np.append(sp,i)   
+    sp=np.sort(np.unique(sp)) # 
+    
+    Kt = 0 # tangent slope
+    s0 = 0
+    dvec = [[0],[0]]
+    vec = [[0],[0]]
+    dtheta=0
+    flag1 = True
+    xcoord = [0]
+    ycoord = [0]
+    for s1 in sp[1:]:
+        ds =s1-s0
+        #if 0 <= s1 <= 2 or 5 <= s1 <= 8: # Fire exposed, circular area
+        if any(start <= s1 <= end for start, end in FS):
+            dtheta = ds/R
+            T = [[np.cos(dtheta), -np.sin(dtheta)],[np.sin(dtheta), np.cos(dtheta)]]
+            if flag1:
+                dvec = np.array([[R*np.sin(dtheta)],[R*(1-np.cos(dtheta))]])
+                flag1=False
+            else:
+                dvec=np.dot(T,dvec)        
+            vec = vec+dvec
+            Kt = dvec[1][0]/dvec[0][0]
+        else: # non-fire, linear area
+            dvec = [[ds],[Kt*ds]]
+            vec = np.add(vec,dvec)
+    
+        s0=s1
+        xcoord.append(float(vec[0][0]))
+        ycoord.append(float(vec[1][0]))
+        
+    xc = np.array(xcoord)*span/xcoord[-1]
+    yc = np.array(ycoord)-ycoord[-1]*xc/span
+        
+    out = {}
+    out['xc']=xc
+    out['yc']=yc
+    out['Umax']=np.min(yc)
+    return out
+
 
 st.header("Concrete deck deflection due to fire")
 datFiles=glob.glob('Data/*.dat')
@@ -30,35 +95,3 @@ with col2:
         default=datFiles
         )
     
-
-
-colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k' ]
-fig2, ax2 = plt.subplots()
-fig2.set_figheight(4)
-for no, datfile in enumerate(data):
-    pl = {'span': [],
-        'maxU': [],
-        'L50':[]}
-    for s in np.linspace(sp_min,sp_max,sp_step):
-        FSinp_p=[(0,s)]
-
-        res1=fp.fdflect(span=s, FS=FSinp_p, Time=ti, datafile=datfile)
-        pl['span'].append(s)
-        pl['maxU'].append(res1['Umax'])
-        pl['L50'].append(-s/50)
-        #res=fdflect(span=sp)
-        #print('span [m]:'+str(sp), 'Umax: '+str(round(res['Umax'],2)), 'L/50 = '+str(sp/50))
-
-    color = colors[no]
-    #st.write(color)
-    plt.plot(pl['span'],pl['maxU'], color=color, lw=1.0, linestyle='solid', label=datfile)
-    #st.write("plot")
-
-
-plt.plot(pl['span'],pl['L50'], color='blue', lw=1.0, linestyle='dotted', label='span/50')
-
-plt.xlabel('Span [m]')
-plt.ylabel('max. Deflection [m]')
-plt.legend()
-plt.grid()
-st.pyplot(fig2)
