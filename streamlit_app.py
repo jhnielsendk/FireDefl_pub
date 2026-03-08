e01d173b458de1f71795d3cd51d7526884664306
jnhr <jnhr@cowi.com>
2026-03-08 14:34:45 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,71 +4,73 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.interpolate import interp1d
 
-def fdflect(Time=3600, 
-            datafile='Data/Ex32.dat',
-            #Height = 0.32,
-            span = 8,
-            Total_points=1200,
-            FS=[],
-            ):
-    
-    Height = float(datafile.split('Ex')[1].split('.')[0])/100
-    #datafile.split('Ex')
+from src.FirePackage import fdflect
 
-    if len(FS) < 1:
-        FS=[(0, span)]
+# def fdflect(Time=3600, 
+#             datafile='Data/Ex32.dat',
+#             #Height = 0.32,
+#             span = 8,
+#             Total_points=1200,
+#             FS=[],
+#             ):
+    
+#     Height = float(datafile.split('Ex')[1].split('.')[0])/100
+#     #datafile.split('Ex')
+
+#     if len(FS) < 1:
+#         FS=[(0, span)]
         
-    data = np.genfromtxt(datafile, names=True)
+#     data = np.genfromtxt(datafile, names=True)
     
-    f_bot = interp1d(data['Time'], data['Bottom'], 'cubic')
-    f_top = interp1d(data['Time'], data['Top'], 'cubic')
+#     f_bot = interp1d(data['Time'], data['Bottom'], 'cubic')
+#     f_top = interp1d(data['Time'], data['Top'], 'cubic')
     
-    delta_strain = f_bot(Time)-f_top(Time)
+#     delta_strain = f_bot(Time)-f_top(Time)
     
-    R = Height/delta_strain
+#     R = Height/delta_strain
        
-    sp=np.linspace(0, span, Total_points)
-    for i in FS:
-        sp=np.append(sp,i)   
-    sp=np.sort(np.unique(sp)) # 
+#     sp=np.linspace(0, span, Total_points)
+#     for i in FS:
+#         sp=np.append(sp,i)   
+#     sp=np.sort(np.unique(sp)) # 
     
-    Kt = 0 # tangent slope
-    s0 = 0
-    dvec = [[0],[0]]
-    vec = [[0],[0]]
-    dtheta=0
-    flag1 = True
-    xcoord = [0]
-    ycoord = [0]
-    for s1 in sp[1:]:
-        ds =s1-s0
-        #if 0 <= s1 <= 2 or 5 <= s1 <= 8: # Fire exposed, circular area
-        if any(start <= s1 <= end for start, end in FS):
-            dtheta = ds/R
-            T = [[np.cos(dtheta), -np.sin(dtheta)],[np.sin(dtheta), np.cos(dtheta)]]
-            if flag1:
-                dvec = np.array([[R*np.sin(dtheta)],[R*(1-np.cos(dtheta))]])
-                flag1=False
-            else:
-                dvec=np.dot(T,dvec)        
-            vec = vec+dvec
-            Kt = dvec[1][0]/dvec[0][0]
-        else: # non-fire, linear area
-            dvec = [[ds],[Kt*ds]]
-            vec = np.add(vec,dvec)
+#     Kt = 0 # tangent slope
+#     s0 = 0
+#     dvec = [[0],[0]]
+#     vec = [[0],[0]]
+#     dtheta=0
+#     flag1 = True
+#     xcoord = [0]
+#     ycoord = [0]
+#     for s1 in sp[1:]:
+#         ds =s1-s0
+#         #if 0 <= s1 <= 2 or 5 <= s1 <= 8: # Fire exposed, circular area
+#         if any(start <= s1 <= end for start, end in FS):
+#             dtheta = ds/R
+#             T = [[np.cos(dtheta), -np.sin(dtheta)],[np.sin(dtheta), np.cos(dtheta)]]
+#             if flag1:
+#                 dvec = np.array([[R*np.sin(dtheta)],[R*(1-np.cos(dtheta))]])
+#                 flag1=False
+#             else:
+#                 dvec=np.dot(T,dvec)        
+#             vec = vec+dvec
+#             Kt = dvec[1][0]/dvec[0][0]
+#         else: # non-fire, linear area
+#             dvec = [[ds],[Kt*ds]]
+#             vec = np.add(vec,dvec)
     
-        s0=s1
-        xcoord.append(float(vec[0][0]))
-        ycoord.append(float(vec[1][0]))
+#         s0=s1
+#         xcoord.append(float(vec[0][0]))
+#         ycoord.append(float(vec[1][0]))
         
-    xc = np.array(xcoord)*span/xcoord[-1]
-    yc = np.array(ycoord)-ycoord[-1]*xc/span
+#     xc = np.array(xcoord)*span/xcoord[-1]
+#     yc = np.array(ycoord)-ycoord[-1]*xc/span
         
-    out = {}
-    out['xc']=xc
-    out['yc']=yc
-    out['Umax']=np.min(yc)
-    return out
+#     out = {}
+#     out['xc']=xc
+#     out['yc']=yc
+#     out['Umax']=np.min(yc)
+#     return out
 
 
 st.header("Concrete deck deflection due to fire")
@@ -96,8 +98,8 @@ with col2:
         default=datFiles
         )
     
-res = fdflect()
-st.write(res['Umax'])
+# res = fdflect()
+# st.write(res['Umax'])
 
 colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k' ]
 fig2, ax2 = plt.subplots()
