0b34982a70b65601c46de760feda87d26f9c1beb
jnhr <jnhr@cowi.com>
2026-03-08 13:24:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Mar  4 11:29:41 2026
-
-@author: JNHR
-"""
 #import matplotlib.pyplot as plt
-import glob
+# import glob
 #import pandas as pd
 
 # from numpy.random import default_rng as rng
@@ -21,18 +15,18 @@ import streamlit as st
 
 st.header("Concrete deck deflection due to fire")
 
-datFiles=glob.glob('Data/*.dat')
+#datFiles=glob.glob('Data/*.dat')
 
-st.divider()
-st.write('Maximum deflection as a function of span. Fire assumed under full span')
+# st.divider()
+# st.write('Maximum deflection as a function of span. Fire assumed under full span')
 
-col1, col2, col3 = st.columns(3)
-with col1:
-     sp_min = st.number_input("Min. Span [m]:", min_value=0.0, value=0.01, step=0.5, format="%.2f")
-with col2:
-     sp_max = st.number_input("Max. Span [m]:", min_value=0.0, value=16.0, step=0.5, format="%.2f")
-with col3:
-     sp_step = st.number_input("no. steps [-]:", min_value=2, value=20, step=1, format="%i")
+# col1, col2, col3 = st.columns(3)
+# with col1:
+#      sp_min = st.number_input("Min. Span [m]:", min_value=0.0, value=0.01, step=0.5, format="%.2f")
+# with col2:
+#      sp_max = st.number_input("Max. Span [m]:", min_value=0.0, value=16.0, step=0.5, format="%.2f")
+# with col3:
+#      sp_step = st.number_input("no. steps [-]:", min_value=2, value=20, step=1, format="%i")
 
 # col1, col2 = st.columns([1, 2])
 # with col1:
